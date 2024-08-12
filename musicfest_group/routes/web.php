68fb6a0836be738a2cb788698686ace37bb12927
koenchavatte <koen.chavatte@student.ehb.be>
2024-08-12 17:13:21 +0200

    rout voor admin account aan te maken op admin dashboard

--- musicfest_group/routes/web.php
+++ musicfest_group/routes/web.php
@@ -5,16 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\AdminController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -35,8 +26,9 @@ Route::middleware([
     })->name('dashboard');
 });
 
-// Admin routes
+
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified', 'admin'])->group(function () {
     Route::get('/admin', [AdminController::class, 'index'])->name('admin.dashboard');
     Route::post('/admin/promote/{user}', [AdminController::class, 'promoteToAdmin'])->name('admin.promote');
+    Route::post('/admin/create', [AdminController::class, 'createAdmin'])->name('admin.create'); // Nieuwe route voor het aanmaken van een admin
 });
